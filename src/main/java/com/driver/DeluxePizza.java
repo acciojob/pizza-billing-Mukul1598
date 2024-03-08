570c69b3a602ba276f43018a7ad9778c6e6325b6
Mukul <mukul.devry99@gmail.com>
2024-03-08 15:41:10 +0530

    answer8

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -5,16 +5,17 @@ public class DeluxePizza extends Pizza {
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
-        addExtraCheese();
-        addExtraToppings();
+        super.addExtraCheese();
+        super.addExtraToppings();
+    }
+
+    @Override
+    public void addExtraCheese() {
+        super.addExtraCheese();
+    }
+
+    @Override
+    public void addExtraToppings() {
+        super.addExtraToppings();
     }
 }
-//    @Override
-//    public void addExtraCheese() {
-//        super.addExtraCheese();
-//    }
-//
-//    @Override
-//    public void addExtraToppings() {
-////        super.addExtraToppings();
-//    }
